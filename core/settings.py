f7abb7d26d2c7a6b74336402bc5a6173916db19b
Ana Maria Gomes <gomesabam@gmail.com>
2025-11-28 09:33:57 -0300

    alterações no settings.py para add static_root

--- core/settings.py
+++ core/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/home/anamariaym/anamariaym.pythonanywhere.com/static/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
